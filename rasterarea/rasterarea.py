d5652c66e8aae8330d0036d69eb9821ffe7f1371
Feng96 <1041267458@qq.com>
2023-04-02 22:11:36 -0400

    add

--- rasterarea/rasterarea.py
+++ rasterarea/rasterarea.py
@@ -4,12 +4,13 @@ import random
 import string
 
 def generate_password(length=8):
-    """
-    This function generates a random password consisting of lowercase and uppercase letters,
-    digits, and special characters.
+    """Generate a random password
 
-    :param length: the length of the password to be generated (default is 8)
-    :return: a string containing the generated password
+    Args:
+        length (int, optional): _description_. Defaults to 8.
+
+    Returns:
+        _type_: _description_
     """
     # Define the set of characters to choose from
     characters = string.ascii_letters + string.digits + string.punctuation
@@ -21,5 +22,13 @@ def generate_password(length=8):
 
 
 def gnerate_lucky_number(length = 1):
+    """Generate your lucky number 
+
+    Args:
+        length (int, optional): _description_. Defaults to 1.
+
+    Returns:
+        _type_: _description_
+    """    
     result_str = ''.join(random.choice(string.digits) for i in range(length))
     return int(result_str)
